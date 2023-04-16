fb0a0511c1a27d8127cd7d802ea18e8d35786f39
Lorenzo Pesce <lorenzo.pesce.1996@gmail.com>
2023-04-16 16:15:16 +0200

    Connect in main menu and show balance sol during play

--- Assets/Samples/Solana SDK/0.0.9/Simple Wallet/Solana Wallet/Scripts/example/screens/GameScreen.cs
+++ Assets/Samples/Solana SDK/0.0.9/Simple Wallet/Solana Wallet/Scripts/example/screens/GameScreen.cs
@@ -1,21 +1,67 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
-using Solana.Unity.Rpc.Types;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using codebase.utility;
+using Cysharp.Threading.Tasks;
+using Solana.Unity.Extensions;
+using Solana.Unity.Rpc.Types;
 
 namespace Solana.Unity.SDK.Example
 {
     public class GameScreen : SimpleScreen
     {
         [SerializeField]
-        private TextMeshProUGUI walletAddress;
+        private TextMeshProUGUI lamports;
+        
+        private CancellationTokenSource _stopTask;
 
         public void Start()
         {
-            //log public key
-            walletAddress.text = Web3.Instance.Wallet.Account.PublicKey;
+            _stopTask = new CancellationTokenSource();
+
+            RefreshWallet();
+            Web3.WsRpc.SubscribeAccountInfo(
+                Web3.Instance.Wallet.Account.PublicKey,
+                (_, accountInfo) =>
+                {
+                    Debug.Log("Account changed!, updated lamport: " + accountInfo.Value.Lamports);
+                    RefreshWallet();
+                },
+                Commitment.Confirmed
+            );
+
+        }
+        
+        private void OnEnable()
+        {
+            Loading.StopLoading();
+        }
+
+        private void RefreshWallet()
+        {
+            UpdateWalletBalanceDisplay().AsUniTask().Forget();
+        }
+        
+        private async Task UpdateWalletBalanceDisplay()
+        {
+            if (Web3.Instance.Wallet.Account is null) return;
+            var sol = await Web3.Base.GetBalance(Commitment.Confirmed);
+            MainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                //log
+                Debug.Log("Wallet balance: " + sol);
+                lamports.text = $"{sol}";
+            });
+        }
+        
+        private void OnDestroy()
+        {
+            if (_stopTask is null) return;
+            _stopTask.Cancel();
         }
     }
 }
